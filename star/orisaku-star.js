4b3f21b3428605f987c74bb47c4f1bd9b742b374
me-no <100861251+me-no@users.noreply.github.com>
2022-04-22 00:32:38 +0900

    Add files via upload
    updated algorithms  for stars

--- star/orisaku-star.js
+++ star/orisaku-star.js
@@ -14,6 +14,8 @@ var scal = 8;
 
 let img;
 
+var noiseVar = 1;
+
 function preload() {
     // Font
     font = loadFont("../assets/misaki_gothic.ttf");
@@ -21,7 +23,7 @@ function preload() {
     // Images
     //img = loadImage("assets/hackle-top.png");
     //img = loadImage("assets/hackle-bottom2.png");
-    img = loadImage("../assets/star/hackle-star-back.png");
+    img = loadImage("../assets/star/hackle-star.png");
 }
 
 function setup () {
@@ -36,8 +38,11 @@ function draw() {
     // 円の半径を決める
     // ランダムな円を108 個生成
     for (k=0; k<108; k++) {
-        x = int(random(0, 128))*8;
+        //noiseVal = noise(noiseVar)*500-200;
+        x = int(random(0, 128))*8;// このx とy で円の左上頂点が決まる→ここを規則的にする
         y = int(random(0, 128))*8;
+        //x = 1024/108 *k;// キャンバスの幅をk等分
+        //y = x+noiseVal;
         r = int(random(2, 15))*2-1;// 奇数で出力
         tr = random(50, 200);
         d = int(random(0,skyColors.length));
@@ -59,10 +64,11 @@ function draw() {
                 }
             }
         }
+        //noiseVar += 0.1;
     }
 
     // ランダムな星を生成_小
-    for (k = 0; k < 5000; k++) {
+    for (k = 0; k < 8000; k++) {
         x = int(random(0, 128)*scal);
         y = int(random(0, 128)*scal);
         r = int(random(1,3));
@@ -74,10 +80,30 @@ function draw() {
         noStroke();
         fill(colr, colg, colb, tr);
         rect(x, y, r/4*scal, r/4*scal);
+
+    }
+    // ランダムな星を生成_星雲
+    starColors.splice(0,1);
+    x_separation = 10000;
+    for (k = 0; k < x_separation; k++) {
+        noiseVal = noise(noiseVar);
+        xx = 1024/x_separation*k;
+        yy = sqrt(xx)*10 +noiseVal*300;
+        r = int(random(1,3));
+        tr = random(50,100);
+        d = int(random(0, starColors.length));
+        colr = starColors[d][0];
+        colg = starColors[d][1];
+        colb = starColors[d][2];
+        noStroke();
+        fill(colr, colg, colb, tr);
+        rect(xx, yy, r/4*scal, r/4*scal);
+
+        noiseVar += 0.4;
     }
 
     // ランダムな星を生成
-    starColors.splice(0,1);
+    //starColors.splice(0,1);
     for (k = 0; k < 500; k++) {
         x = int(random(0, 128)*scal);
         y = int(random(0, 128)*scal);
