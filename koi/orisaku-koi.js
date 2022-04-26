2a94e5bb97cc7dd48d86559dbea3d58cb60d4daf
me-no <100861251+me-no@users.noreply.github.com>
2022-04-26 18:26:17 +0900

    Add files via upload
    add multiple koi

--- koi/orisaku-koi.js
+++ koi/orisaku-koi.js
@@ -24,6 +24,9 @@ var yval = 0;
 var yval2 = 1;
 var yval3 = 0.5;
 var ylefttop = Math.random(1);
+var yleftmid = Math.random(1,2);
+var yrighttop = Math.random(2,3);
+var yrightmid = Math.random(3,4);
 
 let img;
 
@@ -54,37 +57,60 @@ function draw() {
 
     // 左斜一段目
     var xlefttop = 0;
-    for (i = 0; i < 9; i++){
+    for (i = 0; i < 12; i++){
         var y = map(noise(xlefttop, ylefttop), 0, 1, topY_1-4*scal, topY_1+4*scal);
-        var x = ((topX_3-topX_1)/10*i);
-        image(koiye, topX_1 + x, y+(topY_3-topY_1)/(topX_3-topX_1)*x , 18,24);
+        var x = topX_3 - (topX_3-topX_1)/10*i;
+        var wid_i = map(i, 0, 11, 9, 27);
+        var hei_i = map(i, 0, 11, 16, 48);
+        var k = (topY_3-topY_1)/(topX_3-topX_1);
+        image(koiye, x, y+scal*4+k*(x-topX_1) , wid_i, hei_i);
         xlefttop += 0.02;
     }
     ylefttop += 0.01;
 
+    // 左斜2段目
+    var xleftmid = 0;
+    for (i = 0; i < 12; i++){
+        var y = map(noise(xleftmid, yleftmid), 0, 1, topY_1-4*scal, topY_1+4*scal);
+        var x = topX_3 - (topX_3-topX_1)/10*i;
+        var wid_i = map(i, 0, 11, 9, 27);
+        var hei_i = map(i, 0, 11, 16, 48);
+        var k = (topY_3-topY_1)/(2*(topX_3-topX_1));
+        image(koiye, x, y+scal*4+k*(x-topX_1) + height_1/3, wid_i, hei_i);
+        xleftmid += 0.02;
+    }
+    yleftmid += 0.01;
+
+    // 右斜一段目
+    var xrighttop = 0;
+    for (i = 0; i < 12; i++){
+        var y = map(noise(xrighttop, yrighttop), 0, 1, topY_3-4*scal, topY_3+4*scal);
+        var x = topX_3+width_3 + (topX_3-topX_1)/10*i;
+        var wid_i = map(i, 0, 11, 9, 27);
+        var hei_i = map(i, 0, 11, 16, 48);
+        var k = (topY_3-topY_1)/(topX_3-topX_1);
+        image(koiaq, x-36, y+scal*4-k*(x-(topX_3+width_3)), wid_i, hei_i);
+        xrighttop += 0.02;
+    }
+    yrighttop += 0.01;
+
+    // 右斜2段目
+    var xrightmid = 0;
+    for (i = 0; i < 12; i++){
+        var y = map(noise(xrightmid, yrightmid), 0, 1, topY_3-4*scal, topY_3+4*scal);
+        var x = topX_3+width_3 + (topX_3-topX_1)/10*i;
+        var wid_i = map(i, 0, 11, 9, 27);
+        var hei_i = map(i, 0, 11, 16, 48);
+        var k = (topY_3-topY_1)/(2*(topX_3-topX_1));
+        image(koired, x-36, y+scal*4-k*(x-(topX_3+width_3)) + height_3/4, wid_i, hei_i);
+        xrightmid += 0.02;
+    }
+    yrightmid += 0.01;
+
     // 横1列目
-    /*
-    for (i = 0; i < 9; i++) {
-        image(koibl, nisX + (width_1 / 10 *i), topY_1 + nisY , 36, 64);
-        //nisX += noise(noiseValX)-0.4;
-        nisY = noise(noiseValY);
-        noiseValY += 0.02;
-        //noiseValX += 0.02;
-        //console.log(nisY);
-    }*/
-    //beginShape();
     var xval = 0;
     for (i = 0; i < 9; i++) {
         var y =  map(noise(xval, yval), 0, 1, topY_1-4*scal, topY_1+4*scal);
-
-        //Gaussian
-        /*
-        var distX = mouseX - i;
-        var gaussianX = map(distX, -width, width, -5, 5);
-        var gaussianY = map(Gaussian(gaussianX), 0, 0.4, mouseY/4, 0);
-        
-        image(koibl, topX_1 + (width_1 / 10 * i), y + gaussianY, 36,64);
-        */
 
         image(koibl, topX_1 + (width_1 / 10 * i)+y-topY_1, y , 36,64);
         xval += 0.05;
